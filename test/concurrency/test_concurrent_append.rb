7471f12265a261e7bddc2b23b38d3a47ff4f5864
Jacob Kjeldahl <jacob@kjeldahl.it>
2026-03-26 08:42:31 +0100

    Guard on test_concurrent_append is no longer needed
    DcbEventStore::Store awaits refactor before being subjected to mutant testing

--- test/concurrency/test_concurrent_append.rb
+++ test/concurrency/test_concurrent_append.rb
@@ -2,8 +2,6 @@ require_relative "../test_helper"
 require_relative "../support/database"
 require "concurrent"
 
-return if defined?(Mutant)
-
 class TestConcurrentAppend < Minitest::Test
   include DatabaseHelper
 
